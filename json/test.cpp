a83a434dc5303d611305834eb4e5348c97e3c897
ryan <ryan@maideax.com>
2019-10-12 22:39:44 +0800

    Add json

---
+++ json/test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include "json.hpp"
+using json = nlohmann::json;
+using namespace std;
+
+int main()
+{
+    json j;
+    j["pi"] = 3.14;
+    j["is_admin"] = true;
+    j["name"] = "John";
+    j["null"] = nullptr;
+
+    j["obj"] = {{"obj_name", "Tom"}};
+    j["arr"] = {1, 2, 3};
+
+    for (int i = 0; i < 3; i++)
+    {
+        json j_obj;
+        j_obj["x"] = 10;
+        j_obj["y"] = 10;
+        j["obj_arr"].push_back(j_obj);
+    }
+
+    cout << j.dump(4) << endl;
+
+    cout << "---" << endl;
+    json j_parse = json::parse(j.dump(4));
+    cout << j_parse << endl;
+
+    return 0;
+}
