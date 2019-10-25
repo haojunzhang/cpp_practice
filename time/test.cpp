e86008a261bea4ba54a000aaef4a305c033f925e
ryan <ryan@maideax.com>
2019-10-25 18:32:36 +0800

    Update time topic

--- time/test.cpp
+++ time/test.cpp
@@ -4,12 +4,25 @@
 #include <chrono>
 using namespace std;
 using namespace std::chrono;
-string now()
+
+string time_format(time_t time, string format, bool use_utc)
 {
     char buff[20];
-    time_t now = time(nullptr);
-    strftime(buff, 20, "%Y-%m-%d %H:%M:%S", localtime(&now));
+    if (use_utc)
+    {
+        strftime(buff, 20, format.c_str(), gmtime(&time));
+    }
+    else
+    {
+        strftime(buff, 20, format.c_str(), localtime(&time));
+    }
+
     return buff;
+}
+
+string now(bool use_utc)
+{
+    return time_format(time(nullptr), "%Y-%m-%d %H:%M:%S", use_utc);
 }
 
 string unix_time_milliseconds()
@@ -24,9 +37,29 @@ string unix_time_seconds()
     return s.substr(0, s.length() - 3);
 }
 
+time_t str_to_time_t(string str)
+{
+    struct tm tm;
+    strptime(str.c_str(), "%Y-%m-%d %H:%M:%S", &tm);
+    time_t t = mktime(&tm);
+    return t;
+}
+
+string compare_time(string str_time1, string str_time2)
+{
+    time_t time1 = str_to_time_t(str_time1);
+    time_t time2 = str_to_time_t(str_time2);
+    return time1 > time2 ? "after" : (time1 == time2 ? "same" : "before");
+}
+
 int main()
 {
-    cout << "now:" << now() << endl;
+    cout << "now:" << now(false) << endl;
+    cout << "now use utc:" << now(true) << endl;
     cout << "unix time:" << unix_time_milliseconds() << " ms" << endl;
     cout << "unix time:" << unix_time_seconds() << " s" << endl;
+    cout << "2019-10-10 10:10:10 to time_t:" << str_to_time_t("2019-10-10 10:10:10") << endl;
+    cout << "compare time1:" << compare_time("2019-10-10 10:10:10", "2019-10-10 10:10:09") << endl;
+    cout << "compare time2:" << compare_time("2019-10-10 10:10:10", "2019-10-10 10:10:10") << endl;
+    cout << "compare time3:" << compare_time("2019-10-10 10:10:10", "2019-10-10 10:10:11") << endl;
 } // clang++ test.cpp
