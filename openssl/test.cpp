21be87abf99b1b30c2106cb33aa8033490a92e13
ryan <ryan@maideax.com>
2019-10-30 22:55:20 +0800

    Use utils, add rsa

--- openssl/test.cpp
+++ openssl/test.cpp
@@ -1,28 +1,45 @@
 #include <iostream>
 #include <string>
-#include "OpenSSLUtils.h"
-using namespace std;
+#include "../utils/OpenSSLUtils.h"
+#include "../utils/StringUtils.h"
+#include "../utils/FileUtils.h"
 
 int main()
 {
-    string text = "my secret";
+    std::string text = "my secret";
 
     cout << "text:" << text << endl;
 
+    // hash
     cout << "md5:" << OpenSSLUtils::md5(text) << endl;
-
     cout << "sha256:" << OpenSSLUtils::sha256(text) << endl;
 
-    string base64_encode_text = OpenSSLUtils::base64_encode(text);
-
+    // base64
+    std::string base64_encode_text = OpenSSLUtils::base64_encode(text);
     cout << "base64encode:" << base64_encode_text << endl;
-
     cout << "base64decode:" << OpenSSLUtils::base64_decode(base64_encode_text) << endl;
 
-    string enc = OpenSSLUtils::aes_encrypt(text);
-    cout << "aes encrypt:" << enc << endl;
-    string dec = OpenSSLUtils::aes_decrypt(enc);
-    cout << "aes decrypt:" << dec << endl;
+    // aes
+    std::string aes_enc = OpenSSLUtils::aes_encrypt(text);
+    cout << "aes encrypt:" << aes_enc << endl;
+    std::string aes_dec = OpenSSLUtils::aes_decrypt(aes_enc);
+    cout << "aes decrypt:" << aes_dec << endl;
+
+    // rsa
+    std::string rsa_public_key = FileUtils::read_file("pubkey.pem");
+    std::string rsa_private_key = FileUtils::read_file("prikey.pem");
+    std::string rsa_enc = OpenSSLUtils::rsa_encrypt(text, rsa_public_key);
+    std::string rsa_enc_base64 = OpenSSLUtils::base64_encode(rsa_enc);
+    cout << "rsa encrypt(base64):" << rsa_enc_base64 << endl;
+    std::string rsa_dec_base64 = OpenSSLUtils::base64_decode(rsa_enc_base64);
+    std::string rsa_dec = OpenSSLUtils::rsa_decrypt(rsa_dec_base64, rsa_private_key);
+    cout << "rsa decrypt:" << rsa_dec << endl;
 
     return 0;
-} // clang++ test.cpp OpenSSLUtils.cpp -lssl -lcrypto
+}
+/*
+clang++ test.cpp \
+../utils/OpenSSLUtils.cpp \
+../utils/StringUtils.cpp \
+../utils/FileUtils.cpp -lssl -lcrypto
+ */
